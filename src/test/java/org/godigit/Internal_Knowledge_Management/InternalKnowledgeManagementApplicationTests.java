df0a8b7027bcee83c472c54e43a776cf003ef138
Phaneendra2634 <poluphaneendra2004@gmail.com>
2025-08-23 16:33:18 +0530

    initial commit with spring boot project

---
+++ src/test/java/org/godigit/Internal_Knowledge_Management/InternalKnowledgeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.godigit.Internal_Knowledge_Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternalKnowledgeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
