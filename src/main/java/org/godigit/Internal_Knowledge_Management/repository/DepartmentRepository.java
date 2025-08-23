ad2f3c3cd3d166387072c059f4da5b0c81266978
John Mathew <John.Mathew@godigit.com>
2025-08-23 17:58:57 +0530

    Added DepartmentRepository.java

---
+++ src/main/java/org/godigit/Internal_Knowledge_Management/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package org.godigit.Internal_Knowledge_Management.repository;
+
+import org.godigit.ikm.entities.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+  Optional<Department> findByCode(String code);
+}
