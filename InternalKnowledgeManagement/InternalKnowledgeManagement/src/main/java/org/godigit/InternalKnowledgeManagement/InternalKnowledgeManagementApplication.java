d2812f9a80859d5651d900c21f0e5ab96ac99f3b
Phaneendra Babu <132051677+Phaneendra2634@users.noreply.github.com>
2025-08-23 15:11:13 +0530

    Add files via upload

---
+++ InternalKnowledgeManagement/InternalKnowledgeManagement/src/main/java/org/godigit/InternalKnowledgeManagement/InternalKnowledgeManagementApplication.java
@@ -0,0 +1,13 @@
+package org.godigit.InternalKnowledgeManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternalKnowledgeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternalKnowledgeManagementApplication.class, args);
+	}
+
+}
---
+++ InternalKnowledgeManagement/InternalKnowledgeManagement/src/test/java/org/godigit/InternalKnowledgeManagement/InternalKnowledgeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.godigit.InternalKnowledgeManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternalKnowledgeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
